51012b18a2504e7f1f61c2e24f32c4c53074a3d4
Gabrielfer14 <gabrifernande@gmail.com>
2021-10-25 16:27:53 -0300

    termino intervalo

--- intervalos/src/main.cpp
+++ intervalos/src/main.cpp
@@ -16,13 +16,33 @@ using std::endl;
 #include <iomanip>
 using std::setprecision;
 
-double porcento(){
-
-}
 
 int main(void)
 {
-    // TODO: Adicione aqui a sua solução.
+int x, inter1=0,inter2=0,inter3=0,inter4=0,fora=0;
+double total=0;
+while  ( cin >> std::ws >> x) {
+    if( x >= 0 && x < 25){
+        inter1++;
+    }
+    if( x >= 25 && x < 50){
+        inter2++;
+    }
+    if( x >= 50 && x < 75){
+        inter3++;
+    }
+    if( x >= 75 && x < 100){
+        inter4++;
+    }
+    if( x < 0 || x >=100)
+    fora++;
+}
+ total= inter1 + inter2 + inter3 + inter4 + fora;
+ cout << setprecision(4) << 100/(total/inter1) << endl;
+ cout << setprecision(4) << 100/(total/inter2) << endl;
+ cout << setprecision(4) << 100/(total/inter3) << endl;
+ cout << setprecision(4) << 100/(total/inter4) << endl;
+ cout << setprecision(4) << 100/(total/fora) << endl;
 
-    return 0;
+  return 0;
 }
