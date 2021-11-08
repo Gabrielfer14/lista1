cb4ffda7268b02ed74994ae2f98c07d7fc863904
Gabrielfer14 <gabrifernande@gmail.com>
2021-11-08 15:32:55 -0300

    termino fib

--- autor.md
+++ autor.md
@@ -11,10 +11,9 @@ Preencha os dados abaixo para identificar a autoria do trabalho.
 - [x] Negativos 5
 - [x] Soma Vizinhos
 - [x] Intervalos
-- [ ] Fibonacci
+- [x] Fibonacci
 - [x] Minmax
 - [x] Inverter
-- [ ] Filtragem
 - [ ] Ponto em Retângulo 1
 - [ ] Ponto em Retângulo 2
 
--- fibonacci/src/function.cpp
+++ fibonacci/src/function.cpp
@@ -1,9 +1,24 @@
 #include "function.h"
 
+using namespace std;
+
 std::vector<unsigned int> fib_below_n( unsigned int n )
 {
-    // TODO: adicione o seu código aqui.
-
-    // TODO: Isto é apenas um STUB. Troque o retorno pelo que você julgar correto.
-    return std::vector<unsigned int>{};
+   vector<unsigned int> fib;
+   if(n == 0 || n == 1){
+     return fib;
+   }
+   else{   
+     fib.resize(n);
+     fib[0] = 1;
+     fib[1] = 1;
+     int i = 2;
+     while(fib[i-1] + fib[i-2] < n){
+       fib[i] = fib[i-1]+fib[i-2];
+       i++;
+       }
+     fib.erase(remove(begin(fib), end(fib), 0), end(fib));
+     fib.shrink_to_fit();
+     return fib;
+   }
 }
